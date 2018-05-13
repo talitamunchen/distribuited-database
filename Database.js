31b9d26270db8b8ddcba879e507ebd7c7dbdd352
Talita München <tt.munchen@gmail.com>
2018-05-13 01:45:40 -0300

    persisting database to filesystem

--- Database.js
+++ Database.js
@@ -1,24 +1,41 @@
 'use strict';
 const uuid = require('uuid/v4'); //blibioteca para gerar chaves unicas - versao 4
+const fs = require('fs');
 
 module.exports = class Database {
     constructor () {
         this.datamap = {}; //inicializa a database em memória
+        const files = fs.readdirSync(`${process.env.DATA_DIR}`);
+        files.forEach(file => {
+            //lendo os documento do disco, transformando em objeto
+            const data = JSON.parse(fs.readFileSync(`${process.env.DATA_DIR}\\${file}`, {encoding: 'utf8'}));
+            //lendo do disco e joga na memória
+            this.datamap[data.id] = data; 
+        });
     }
 
     insert (data) {
         const id = uuid(); //gera um id
         data.id = id;
         this.datamap[id] = data; //insere id no map
+        //escrevendo em disco
+        fs.writeFileSync(`${process.env.DATA_DIR}\\${id}`, JSON.stringify(data), {encoding: 'utf8'});
         return data;
     }
 
     delete (id) {
         delete this.datamap[id];
+        fs.unlink(`${process.env.DATA_DIR}\\${id}`, function(err){
+            if (err){
+                console.log(`Erro ao deletar o arquivo ${err}`);
+            }
+        });
     }
 
     update (data) {
         this.datamap[data.id] = data; //sobrescre o documento na posição data do id especifico com o data que vem do parametro
+        //sobrescrevendo em disco
+        fs.writeFileSync(`${process.env.DATA_DIR}\\${data.id}`, JSON.stringify(data), {encoding: 'utf8'});
     }
 
     find (query) {
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'; //utilizando es6, ultima versão do javascript
+require('dotenv').config(); //configura as variaveis de ambiente
+
 const Database = require('./Database');
-
 const d = new Database();
-
+/*
 const maria = d.insert({
     nome: 'Maria',
     idade: 12
@@ -20,15 +21,13 @@ const jose = d.insert({
 
 const joao2 = d.insert({
     nome: 'Joao',
-    idade: 12
+    idade: 13
 });
 
 const joaoSilva = d.insert({
     sobrenome: 'Silva'
 });
+*/
 
-d.print();
-
-console.log(JSON.stringify(d.find({
-    nome: "Joao"
-})));
+d.delete('40d42d67-ae54-4779-a18c-138e7c193cad');
+//console.log(JSON.stringify(d.find({})));
